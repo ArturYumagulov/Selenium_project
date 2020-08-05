0373c26116382a0fbe6be05532c8d69d1e661107
YumagulovA <zico.13288@gmail.com>
2020-08-05 12:54:36 +0300

    2.4.1

--- 2.4/task_1.py
+++ 2.4/task_1.py
@@ -1,18 +1,27 @@
-import time
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+import math
+import time
 
-link = "http://suninjuly.github.io/wait1.html"
+link = "http://suninjuly.github.io/explicit_wait2.html"
 
 
 def sel_task(link):
     try:
         browser = webdriver.Chrome()
-        browser.implicitly_wait(5)
+
         browser.get(link)
-        button = browser.find_element_by_css_selector('button').click()
-        message = browser.find_element_by_id('verify_message').text
-        assert message == "Verification was successful!"
+        price = WebDriverWait(browser, 12).until(expected_conditions.text_to_be_present_in_element((By.ID, 'price'),
+                                                                                                   '100'))
+        button = browser.find_element_by_id('book').click()
+
+        digit = browser.find_element_by_id("input_value").text
+        answer = browser.find_element_by_id('answer').send_keys(str(math.log(abs(12 * math.sin(int(digit))))))
+        submit = browser.find_element_by_id('solve').click()
     finally:
+        time.sleep(10)
         browser.quit()
 
 
