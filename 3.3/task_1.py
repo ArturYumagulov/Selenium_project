160d7756ea5b03599a414f9469f1997f84a1b593
YumagulovA <zico.13288@gmail.com>
2020-08-06 11:08:34 +0300

    3.3.1

---
+++ 3.3/task_1.py
@@ -0,0 +1,44 @@
+import unittest
+from selenium import webdriver
+
+
+class Registration:
+    def __init__(self):
+        self.browser = webdriver.Chrome()
+
+    def start(self, link):
+        try:
+            self.browser.get(link)
+            self.browser.implicitly_wait(5)
+            first_name = self.browser.find_element_by_xpath(
+                "html/body/div/form/div[@class='first_block']/div[@class='form-group first_class']/input")\
+                .send_keys("first_name")
+            last_name = self.browser.find_element_by_xpath("html/body/div/form/div[@class='first_block']/"
+                                                  "div[@class='form-group second_class']/input")\
+                .send_keys("last_name")
+            email = self.browser.find_element_by_xpath("html/body/div/form/div[@class='first_block']/"
+                                              "div[@class='form-group third_class']/input")\
+                .send_keys("email")
+            submit = self.browser.find_element_by_css_selector('button.btn').click()
+            text = self.browser.find_element_by_css_selector('h1').text
+
+        finally:
+            self.browser.quit()
+
+
+class TestAbs(unittest.TestCase):
+
+    def test_reg2(self):
+        x = Registration()
+        self.assertEqual(x.start("http://suninjuly.github.io/registration1.html"), None, "Should be absolute value od number")
+
+    def test_reg3(self):
+        x = Registration()
+        self.assertEqual(x.start("http://suninjuly.github.io/registration1.html"), None, "Should be absolute value od number")
+
+    def test_reg1(self):
+        x = Registration()
+        self.assertEqual(x.start("http://suninjuly.github.io/registration2.html"), None, "Should be absolute value od number")
+
+# if __name__ == '__main__':
+#     unittest.main()
